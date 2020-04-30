70ba270bbe9bf58db0b0f151d9ea29305549e4e3
rivu <58026024+unphydra@users.noreply.github.com>
2020-04-30 14:58:14 +0530

    added add_to_end function defination, it will add new node at the end of the list

--- list.c
+++ list.c
@@ -16,3 +16,17 @@ List_ptr create_list()
   new_list->count = 0;
   return new_list;
 }
+
+Status add_to_end(List_ptr list, int value)
+{
+  Node_ptr new_node = create_node(value);
+  if(list->head == NULL){
+    list->head = new_node;
+  } else
+  {
+    list->last->next = new_node;
+  }
+  list->last = new_node;
+  list->count++;
+  return Success;
+}
