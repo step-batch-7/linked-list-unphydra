e0616eb9fe1ff5bdd5d20951b55547fc00eaf1fb
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-01 14:32:26 +0530

    added test for add_to_end function

--- test.c
+++ test.c
@@ -1,7 +1,43 @@
 #include "list.h"
 
+typedef int * Array;
+typedef char * String;
+
+Status compare_list_values(List_ptr,Array,int);
+void show_message(String,Status);
+
+Status compare_list_values(List_ptr list, Array a, int length)
+{
+  Status s = Success;
+  s = list->count == length;
+  Node_ptr p_walk = list->head;
+  int count = 0;
+  while (p_walk!=NULL)
+  {
+    s = s && p_walk->value == a[count];
+    count++;
+    p_walk = p_walk->next;
+  }
+  return s && (count == length);
+}
+
+void show_message(String msg, Status s)
+{
+  printf("%s => %s\n",msg,s?"pass":"fails");
+}
+
+void test_add_to_end(void)
+{
+  List_ptr actual = create_list();
+  Status s = add_to_end(actual,5);
+  int expected[] = {5};
+  int expected_length = 1;
+  s = s && compare_list_values(actual,expected,expected_length);
+  show_message("should add a value if the list head is null", s);
+}
+
 int main(void)
 {
-  printf("hello");
+  test_add_to_end();
   return 0;
 }
