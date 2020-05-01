cc7456d7fedad010bcdde539cc24aed5362bc201
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-01 14:41:55 +0530

    added more test for add_to_end function

--- test.c
+++ test.c
@@ -30,10 +30,16 @@ void test_add_to_end(void)
 {
   List_ptr actual = create_list();
   Status s = add_to_end(actual,5);
-  int expected[] = {5};
-  int expected_length = 1;
-  s = s && compare_list_values(actual,expected,expected_length);
+  int expected_1[] = {5};
+  int expected_1_length = 1;
+  s = s && compare_list_values(actual,expected_1,expected_1_length);
   show_message("should add a value if the list head is null", s);
+
+  s = add_to_end(actual,2);
+  int expected_2[] = {5,2};
+  int expected_2_length = 2;
+  s = s && compare_list_values(actual,expected_2,expected_2_length);
+  show_message("should add a value at the end of the list", s);
 }
 
 int main(void)
