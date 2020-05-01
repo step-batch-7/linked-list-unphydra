6d76dc3ec3cd3287370ead18c177cb0c897fc050
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-01 14:43:52 +0530

    added test for add_to_start function

--- test.c
+++ test.c
@@ -42,8 +42,26 @@ void test_add_to_end(void)
   show_message("should add a value at the end of the list", s);
 }
 
+void test_add_to_start()
+{List_ptr actual = create_list();
+  Status s = add_to_start(actual,5);
+  int expected_1[] = {5};
+  int expected_1_length = 1;
+  s = s && compare_list_values(actual,expected_1,expected_1_length);
+  show_message("should add a value if the list head is null", s);
+
+  s = add_to_start(actual,2);
+  int expected_2[] = {2,5};
+  int expected_2_length = 2;
+  s = s && compare_list_values(actual,expected_2,expected_2_length);
+  show_message("should add a value at the start of the list", s);
+
+}
+
+
 int main(void)
 {
   test_add_to_end();
+  test_add_to_start();
   return 0;
 }
