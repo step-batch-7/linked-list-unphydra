5f6911071907d8e5dbf23d16dd5a32577b2cc35a
rivu <58026024+unphydra@users.noreply.github.com>
2020-05-01 18:41:54 +0530

    added show menu function that will show the menu of operations

--- numbers.c
+++ numbers.c
@@ -1,6 +1,13 @@
 #include "list.h"
 
+void show_menu(void);
+
+void show_menu(void){
+  printf("Main Menu\n---------\n(a) add a number to the end of the list\n(b) add a number to the start of the list\n(c) insert a number at a given position in the list\n(d) add a unique item on the list at the end\n(e) remove a number from the beginning of the list\n(f) remove a number from the end of the list\n(g) remove a number from a given position in the list\n(h) remove first occurrence of a number\n(i) remove all occurrences of a number\n(j) clear the whole list\n(k) check if a number exists in the list\n(l) display the list of numbers\n(m) exit\n\nPlease enter the alphabet of the operation you would like to perform\n");
+}
+
 int main(void)
 {
+  show_menu();
   return 0;
 }
